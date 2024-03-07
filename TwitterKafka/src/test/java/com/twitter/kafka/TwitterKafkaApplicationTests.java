9d40e3ccaf8f2968c2a9c2357a70245fd8ad0e09
Sudeep Singh <sudeepsingh@Sudeeps-MacBook-Pro.local>
2024-03-07 16:34:20 +0530

    Creator commit

---
+++ TwitterKafka/src/test/java/com/twitter/kafka/TwitterKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
